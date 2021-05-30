63e9271fd4096f60d7b660bf20a6b5fcd3023ae5
ranjan <ranjanbajra@gmail.com>
2021-05-30 12:05:24 +0545

    project setup

---
+++ app/models/user.rb
@@ -0,0 +1,20 @@
+class User
+  include Mongoid::Document
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  field :email,              type: String, default: ""
+  field :encrypted_password, type: String, default: ""
+  field :first_name, type: String, default: ""
+  field :last_name, type: String, default: ""
+  field :reset_password_token,   type: String
+  field :reset_password_sent_at, type: Time
+  field :remember_created_at, type: Time
+
+  validates_presence_of :first_name, :last_name, :email
+  validates_uniqueness_of :email, :case_sensitive => false
+  attr_accessor :first_name,:last_name, :email, :password, :password_confirmation, :remember_me
+
+end
